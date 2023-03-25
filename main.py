b4bc27145661c6a3043c555ee1846f451403c303
jaswindersingh1998 <95260140+jaswindersingh1998@users.noreply.github.com>
2023-03-24 21:53:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ with st.container():
     st.markdown(
         "This Histogram display the total number of word in each reviews."
     )
-    image = Image.open('C:\\Users\\HP\\PycharmProjects\\sentimentanalysis.py\\positive_words.png')
+    image = Image.open('positive_words.png')
     st.image(image)
 
 with st.container():
@@ -163,14 +163,14 @@ with st.container():
     st.markdown(
         "This graph illustrates the most common positive words that are found in the dataset."
     )
-    image = Image.open('C:\\Users\\HP\\PycharmProjects\\sentimentanalysis.py\\newplot.png')
+    image = Image.open('newplot.png')
     st.image(image)
 with st.container():
     st.header("Most Common Negative words")
     st.markdown(
         "This graph illustrates the most common negative words that are found in the dataset."
     )
-    image = Image.open('C:\\Users\\HP\\PycharmProjects\\sentimentanalysis.py\\bar.png')
+    image = Image.open('bar.png')
     st.image(image)
 
 with st.container():
@@ -178,7 +178,7 @@ with st.container():
     st.markdown(
         "This WordCloud display the most common positive words that we find in the dataset. WordCloud helps to analyze the test data through visualization and importance of the words can be explained by its frequency."
     )
-    image = Image.open('C:\\Users\\HP\\PycharmProjects\\sentimentanalysis.py\\positive.png')
+    image = Image.open('positive.png')
     st.image(image)
 
 with st.container():
@@ -186,7 +186,7 @@ with st.container():
     st.markdown(
         "This is the WordCloud for most common negative words."
     )
-    image = Image.open('C:\\Users\\HP\\PycharmProjects\\sentimentanalysis.py\\word.png')
+    image = Image.open('word.png')
     st.image(image)
 
 
